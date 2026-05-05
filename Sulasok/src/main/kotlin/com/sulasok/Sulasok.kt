21a9ce1d199a9e77ee20e478cb016bc0d045c918
IstarVin <ajdeleverio20@gmail.com>
2026-05-05 21:01:12 +0800

    base

--- Sulasok/src/main/kotlin/com/sulasok/Sulasok.kt
+++ Sulasok/src/main/kotlin/com/sulasok/Sulasok.kt
@@ -1,7 +1,21 @@
 package com.sulasok
 
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.mainPageOf
 
 open class Sulasok : MainAPI() {
-
+    override var mainUrl = "https://sulasokvid.xyz"
+    override var name = "Sulasok"
+    override val hasMainPage = true
+    override var supportedTypes = setOf(TvType.Movie)
+    override var lang = "en"
+    override val mainPage = mainPageOf(
+        "trending" to "Trending",
+        "movies" to "Movies",
+        "genre/comedy" to "Comedy",
+        "genre/romance" to "Romance",
+        "genre/action" to "Action",
+        "genre/digitally-restored" to "Digitally Restored"
+    )
 }
---
+++ Sulasok/src/main/kotlin/com/sulasok/SulasokPlugin.kt
@@ -0,0 +1,12 @@
+package com.sulasok
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+
+@CloudstreamPlugin
+class SulasokPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Sulasok())
+    }
+}
